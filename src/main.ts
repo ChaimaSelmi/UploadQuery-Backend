76e1f0bd907a633aed8d36002bfb271db7c0f15a
ChaimaSelmi <chaima.selmi2000@gmail.com>
2025-02-14 20:11:23 +0100

    Fix Vercel main.ts path

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Activer CORS pour toutes les origines
+  app.enableCors({
+    origin: 'https://upload-query-frontend.vercel.app'
+  }); 
   await app.listen(3001);
 }
 bootstrap();
