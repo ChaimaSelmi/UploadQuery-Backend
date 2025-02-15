24a7e39a96d0ba90d18725142ee445e5636adcfc
ChaimaSelmi <chaima.selmi2000@gmail.com>
2025-02-15 17:52:59 +0100

    main.ts modifié

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://uploadquery-frontend-2.onrender.com'],
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  });
-  
+  app.use(cors());
   await app.listen(3001);
-
 }
 bootstrap();
